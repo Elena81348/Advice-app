012691d2c3b147b3c8ef21f140b4dc53ffd4229f
“@Elena81348” <fox8134836@gmail.com>
2023-02-05 16:14:45 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect,useState,useRef } from 'react';
+import video from './video.mp4'
+import song from'./music.mp3'
 
 function App() {
+const [advice, setAdvice]=useState('');
+const refAudio=useRef();
+const[paused, setPaused]=useState(true);
+
+useEffect(()=>{
+getAdvice()
+},[])
+
+    const getAdvice=async()=>{
+    const responce=await fetch('http://www.boredapi.com/api/activity/ ')
+    const data=await responce.json()
+    console.log(data.activity)
+    setAdvice(data.activity)
+  }
+  const handleplay=()=>{
+    setPaused(!paused);
+    paused?refAudio.current.play():refAudio.current.pause();
+  }
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <video autoPlay muted loop>
+          <source src={video} type='video/mp4'/>
+        </video>
+      </div>
+      <div>
+        <audio  ref={refAudio} src={song}loop='loop'>
+        </audio>
+        <button className='cta' onClick={handleplay}>
+          {paused?'play':'pause'}
+        </button>
+      </div>
+      <div className='container'>
+        <p>{advice}</p>
+      </div>
+      <div className='container'>
+        <button onClick={getAdvice}>New tip</button>
+      </div>
     </div>
   );
 }
